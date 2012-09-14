3b8fc82dc13dec45d5081fa83394812c40d8d652
Mike Thomas <contact@atomicrobotdesign.com>
2012-09-14 16:10:49 -0500

    Move Slider to own file and allow it to be called and configured from html file

---
+++ js/Slider.js
@@ -0,0 +1,58 @@
+var Slider = function(s, t) {
+			
+	var slider = $(s);
+	var imgs = $(s + ' img');
+	var curImg = 0;
+	var timer = 0;
+	var slideTime = (t) ? t : 3000;
+
+	if ($('html').hasClass('no-opacity')) {
+		for (var i = 0; i < imgs.length; i++) {
+			$(imgs[i]).css('opacity', '0');
+		}
+		$(imgs[0]).css('opacity','1');
+	} else {
+		$(imgs[0]).addClass('show');
+	}
+
+	function slideShowCSS() {
+		if (timer < slideTime) {
+			timer += 10;
+		} else if (timer >= slideTime) {
+			if (curImg < imgs.length - 1) {
+				$(imgs[curImg]).removeClass('show');
+				curImg += 1;
+				$(imgs[curImg]).addClass('show');
+			} else {
+				$(imgs[curImg]).removeClass('show');
+				curImg = 0;
+				$(imgs[curImg]).addClass('show');
+			}
+			timer = 0;
+		}
+	}
+
+	function slideShowjQuery() {
+		if (timer < slideTime) {
+			timer += 10;
+		} else if (timer >= slideTime) {
+			if (curImg < imgs.length - 1) {
+				$(imgs[curImg]).animate({'opacity': '0'}, slideTime);
+				curImg += 1;
+				$(imgs[curImg]).animate({'opacity': '1'}, slideTime);
+			} else {
+				$(imgs[curImg]).animate({'opacity': '0'}, slideTime);
+				curImg = 0;
+				$(imgs[curImg]).animate({'opacity': '1'}, slideTime);
+			}
+			timer = 0;
+		}
+	}
+
+	if ($('html').hasClass('csstransitions')) {
+		setInterval(slideShowCSS, 20);
+	} else if ($('html').hasClass('no-csstransitions')) {
+		setInterval(slideShowjQuery, 20);
+	}
+
+}
